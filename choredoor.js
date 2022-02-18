c17226548840d003ec78cfccd2fc6a1bfc7f8fc8
thedy wender <thedywender1@gmail.com>
2022-02-18 14:39:48 -0400

    fixed initial fails

--- choredoor.js
+++ choredoor.js
@@ -1,19 +1,43 @@
-let doorImage1 = document.getElementById("door1");
-let doorImage2 = document.getElementById("door2");
-let doorImage3 = document.getElementById("door3");
+let doorImage1 = document.getElementById('door1');
+let doorImage2 = document.getElementById('door2');
+let doorImage3 = document.getElementById('door3');
 
 let botDoorPath = "https://content.codecademy.com/projects/chore-door/images/robot.svg"
 let beachDoorPath = "https://content.codecademy.com/projects/chore-door/images/beach.svg"
 let spaceDoorPath = "https://content.codecademy.com/projects/chore-door/images/space.svg"
 
-door1.onclick = () => {
-  doorImage1.src = botDoorPath;
+let numClosedDoors = 3;
+let openDoor1;
+let openDoor2;
+let openDoor3;
+
+let randomChoreDoorGenerator = () => {
+    choreDoor = Math.floor(Math.random() * numClosedDoors);
+    if (choreDoor === 0) {
+        openDoor1 = botDoorPath;
+        openDoor2 = beachDoorpath;
+        openDoor3 = spaceDoorPath;
+    } else if (choreDoor === 1) {
+        openDoor2 = beachDoorPath;
+        opendoor1 = botDoorPath;
+        openDoor3 = spaceDoorPath;
+    } else {
+        (choreDoor === 2)
+        openDoor3 = beachDoorpath;
+        openDoor2 = botDoorpath;
+        openDoor1 = sapceDoorPath;
+    }
+
 }
 
-door2.onclick = () => {
-  doorImage2.src = beachDoorPath;
+doorImage1.onclick = () => {
+    doorImage1.src = openDoor1;
+}
+doorImage2.onclick = () => {
+    doorImage2.src = openDoor2;
+}
+doorImage3.onclick = () => {
+    doorImage3.src = openDoor3;
 }
 
-door3.onclick = () => {
-  doorImage3.src = spaceDoorPath;
-}
+randomChoreDoorGenerator();
